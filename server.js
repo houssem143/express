2da17a0376ec6dc61cc398e62ce8522338971428
houssem143 <houssem14392@gmail.com>
2023-01-08 17:59:19 +0100

    exp

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const date=new Date()
 const day=date.getDay()
 const hour= date.getHours()
 
-if ((day==1 || day==2) && (hour<=9 || hour>=20)) {
+if ((day==6 || day==0) || (hour<=9 || hour>=17)) {
   res.send("Error:Sorry retry later!")
 }else
   (next())
